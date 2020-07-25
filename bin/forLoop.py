e6e9147adf5dad1814d7ac1090e8b4279b6ba96b
t0nym <jamtone@blueyonder.co.uk>
2020-07-25 17:22:15 +0100

    Added new script exercise-another version

---
+++ bin/forLoop.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+###     Print a column of numbers
+print()
+for numbers in range (4):
+    print(numbers)
+    
+distro = ["ubuntu", "mint", "elementary"]
+for xyz in distro:
+    print (xyz)
+    
+for xyz in "ubuntu":
+    print(xyz)
+    
+for xyz in distro:
+    print (xyz)
+    if xyz == "mint":
+        break
+    
+for xyz in distro:
+    if xyz == "mint"
+     continue
+    print (xyz)
+    
+linux = ["secure", "superior"]
+for x in distro:
+   for y in linux:
+     print (x,y)
